5be68bbdf1da123755a8340fbc71d183e37ecb08
NabilahRahmahHasibuan <nabilahhasibuan4@gamil.com>
2023-12-04 20:05:32 +0700

    1

---
+++ src/components/Cardbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Cardbar() {
+  return (
+    <div>
+      <div classname="card text-bg-dark">
+  <img src="src/assets/bg.png" classname="card-img" ></img>
+  <div classname="card-img-overlay">
+    <div classname="text" style={{marginTop: '-500px', marginLeft:'100px'}}> 
+    <h4 classname="card-title"  style={{marginLeft:'28rem'}}><img src="src/assets/daun.png"  style={{width: '42px'}} classname="position-absolute top10 start40 translate-middle-x"></img></h4>
+    <h1 classname="card-title" style={{color: '#F7C35F', fontSize: '65px'}}><b>Siap membantu</b></h1>
+    <h1 classname="card-title"style={{ color: '#ffff' , fontSize:'65px'}}><b>Anda dalam berkebun</b></h1>
+    <p classname="card-text" style={{ color: '#ffff' , fontSize:'20px'}}>Memberikan panduan berkebun dilahan yang
+    <br></br>
+    minim dengan metode Hidroponik dan Polybag</p>
+  </div>
+</div>
+</div>
+</div>
+  )
+}
+
+export default Cardbar
---
+++ src/components/Sectentang.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Sectentang() {
+  return (
+
+<section class="tentang" style={{marginTop:'15rem'}}>
+<h1><p>Tentang Kami</p></h1>
+<p>Temukan Informasi website dan app kami disini</p>
+</section>
+
+)
+}
+
+export default Sectentang
