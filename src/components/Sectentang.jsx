ed3276c9307b80b74d88944b5d888a875f3e81aa
NabilahRahmahHasibuan <nabilahhasibuan4@gamil.com>
2023-12-04 21:53:22 +0700

    1

--- src/components/Sectentang.jsx
+++ src/components/Sectentang.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SecTentang() {
   return (
 
-<section class="tentang" style={{marginTop:'18rem'}}>
+<section class="tentang" style={{marginTop:'18rem', marginLeft:'2rem'}}>
 <h1><p>Tentang Kami</p></h1>
 <p>Temukan Informasi website dan app kami disini</p>
 </section>
